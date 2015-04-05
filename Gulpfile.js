0fc3a56142b94c7cf640ea4432a3b0282207f27f
Sean Wilson <sw@seanw.org>
2015-04-05 20:25:23 +0100

    Update 2015-04-05T19:25:22.908Z

---
+++ Gulpfile.js
@@ -0,0 +1,157 @@
+var config = {
+  paths: {
+    build: "./_site/**",
+    img: ["./img/**/*"],
+    js: ["./js/**/*.js"],
+    css: ["./css/**/*.css"],
+    html: {
+      src: ["./_site/**/*.html"],
+      dest: "./build"
+    },
+  },
+};
+var secretConfig = require('./secret-config.js')(config);
+
+var uglify = require('gulp-uglifyjs');
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var concat = require('gulp-concat');
+var reload = browserSync.reload;
+var minifyCSS = require('gulp-minify-css');
+var ghPages = require('gulp-gh-pages');
+var rimraf = require('gulp-rimraf');
+var runSequence = require('run-sequence');
+var minifyHTML = require("gulp-minify-html");
+var imagemin = require('gulp-imagemin');
+var http = require('http');
+var cloudflare = require('cloudflare').createClient(config.cloudflareAccount);
+var less = require('gulp-less');
+
+gulp.task('imagemin', function() {
+  return gulp.src(config.paths.img, {
+      base: './'
+    })
+    .pipe(imagemin({
+      progressive: true
+    }))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('clean', function(cb) {
+  // rimraf(config.paths.build, cb);
+  return gulp.src(config.paths.build, {
+      read: false
+    }) // much faster
+    .pipe(rimraf());
+});
+
+gulp.task('upload', function() {
+  return gulp.src(config.paths.build)
+    .pipe(ghPages(config.ghPages));
+});
+
+gulp.task('deploy', function() {
+  return runSequence(['html', 'css'], 'upload', 'purge-online-cache', 'submit-sitemap');
+});
+
+gulp.task('submit-sitemap', function(cb) {
+  require('submit-sitemap').submitSitemap(config.sitemapUrl, function(err) {
+    if (err)
+      console.warn(err);
+
+    cb();
+  });
+});
+
+// Purges website cache so updates are shown
+gulp.task('purge-online-cache', function() {
+  cloudflare.clearCache(config.siteDomain, 1, function() {});
+});
+
+gulp.task("html", function() {
+  // Overwrite original files
+  return gulp.src(config.paths.html.src, {
+      base: './'
+    })
+    .pipe(minifyHTML())
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task("css", function() {
+  return gulp.src("css/*.css", {
+      base: './'
+    })
+    .pipe(less().on('error', function (err) {
+      console.log(err);
+    }))
+    .pipe(minifyCSS())
+    .pipe(concat('styles.css'))
+    .pipe(gulp.dest('_site/css/'));
+});
+
+gulp.task('js', function() {
+  return gulp.src('js/*.js')
+  .pipe(uglify())
+  .pipe(concat('scripts.js'))
+  .pipe(gulp.dest('_site/js/'));
+});
+
+// Compile SASS & auto-inject into browsers
+gulp.task('sass', function() {
+  return gulp.src('scss/styles.scss')
+    .pipe(sass({
+      includePaths: ['scss']
+    }))
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+// Reload all Browsers
+gulp.task('bs-reload', function() {
+  browserSync.reload();
+});
+
+function jekyllBuild(done) {
+  return require('child_process', done).exec('jekyll build --trace', {
+    stdio: 'inherit'
+  }, done);
+}
+
+gulp.task('commit-and-deploy', function(done) {
+  return require('child_process', done).exec('git add . && git commit -m "..." && git push && gulp deploy', {
+    stdio: 'inherit'
+  }, done);
+});
+
+gulp.task('jekyll', function(done) {
+  jekyllBuild(done);
+});
+
+// Start the server
+gulp.task('browser-sync', function() {
+  browserSync({
+    server: {
+      baseDir: "./_site/"
+    },
+    open: false
+  });
+});
+
+gulp.task('build', function(done) {
+  runSequence('jekyll', ['css', 'js'], 'bs-reload', done);
+});
+
+gulp.task('default', ['build', 'browser-sync'], function() {
+//gulp.task('default', function() {
+  //gulp.watch(config.paths.html.src, ['html']);
+  //gulp.watch(config.paths.css, runSequence('jekyll', 'css', 'bs-reload'));
+
+  //gulp.watch(["./js/*.js", "./**/*.html", "./css/*.css"],   runSequence('jekyll', 'css', 'bs-reload'));
+  gulp.watch(["./js/*.js", "./**/*.html", "./css/*.css", "./**/*.markdown", "./**/*.yml", "img/**/*", "!./build/**/*", "!./_site/**/*"], ['build']);
+
+
+  //gulp.watch("./build/**/*", ['bs-reload']);
+  //gulp.watch(["./js/*.js", "./**/*.html", "!./build/**/*", "!./_site/**/*"], ['jekyll-build', 'bs-reload']);
+});
---
+++ js/9.custom.js
@@ -0,0 +1,95 @@
+// var wow = new WOW({
+//   boxClass: 'wow',
+//   animateClass: 'animated',
+//   mobile: false,
+//   offset: 120
+// });
+// wow.init();
+
+$('form').validate({
+  rules: {
+    '_replyto': {
+      required: true
+    }
+  },
+  errorPlacement: function(error, element) {
+    error.insertBefore(element);
+  },
+  submitHandler: function(form) {
+
+    form.submit();
+  }
+});
+
+var options = {
+  debug: true
+};
+
+function log(message) {
+  if (options.debug) {
+    console && console.log && console.log(message);
+  }
+}
+
+function analyticsEvent(category, action, label, value, callback) {
+  var event = {
+    'hitType': 'event',
+    'eventCategory': category,
+    'eventAction': action,
+    'eventLabel': label,
+    'eventValue': value,
+    'hitCallback': callback
+  };
+
+  log('Sending event: ' + category + ", " + action + ", " + label + ", " + value);
+  ga('send', event);
+
+  // TODO: Adblockers break the callback
+}
+
+function trackLinkClick(element) {
+  var handler = function(event) {
+    event.preventDefault();
+    var element = $(this);
+
+    analyticsEvent('Link', 'Click', element.attr('href'), undefined, function() {
+      // Remove our handler so we can trigger the previous event handler
+      element.unbind('click', handler);
+      element[0].click();
+      // Restore our handler to track if the element is clicked again
+      element.bind('click', handler);
+    });
+  };
+  element.bind('click', handler);
+}
+
+function trackSubmit(element) {
+  // Wrapping the submit function means we only track when the form actually gets submitted,
+  // otherwised submissions cancelled by validation failures will be tracked also.
+  var oldSubmit = element[0].submit;
+  element[0].submit = function() {
+    analyticsEvent('Form', 'Submit', element.attr('id'), undefined, function() {
+      oldSubmit.apply(element[0], arguments);
+    });
+  };
+}
+
+var trackElements = function(selector) {
+  if (typeof ga === 'undefined')
+    log("Google Analytics not loaded!");
+
+  log('Init tracking');
+
+  var elements = $(selector).each(function(i, element) {
+    element = $(element);
+
+    if (element.is('form')) {
+      trackSubmit(element);
+    }
+    else if (element.is('a')) {
+      trackLinkClick(element);
+    }
+  });
+};
+
+//trackElements('form,a');
