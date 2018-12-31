2aa2557df11c39fb63d3972935790dc78d01f0a4
Sean Wilson <sw@seanw.org>
2018-12-31 18:25:11 +0000

    Update 2018-12-31T18:25:06.243Z

--- Gulpfile.js
+++ Gulpfile.js
@@ -7,145 +7,169 @@ var config = {
     html: {
       src: ["./_site/**/*.html"],
       dest: "./build"
-    },
-  },
+    }
+  }
 };
 
-var secretConfig = require('./_secret-config.js')(config);
-var args = require('yargs').argv;
-var uglify = require('gulp-uglifyjs');
-var gulp = require('gulp');
-var gulpIf = require('gulp-if');
-var browserSync = require('browser-sync');
-var concat = require('gulp-concat');
+var secretConfig = require("./_secret-config.js")(config);
+var args = require("yargs").argv;
+var uglify = require("gulp-uglifyjs");
+var gulp = require("gulp");
+var gulpIf = require("gulp-if");
+var browserSync = require("browser-sync");
+var concat = require("gulp-concat");
 var reload = browserSync.reload;
-var minifyCSS = require('gulp-minify-css');
-var ghPages = require('gulp-gh-pages');
-var rimraf = require('gulp-rimraf');
-var runSequence = require('run-sequence');
+var minifyCSS = require("gulp-minify-css");
+var ghPages = require("gulp-gh-pages");
+var rimraf = require("gulp-rimraf");
+var runSequence = require("run-sequence");
 var minifyHTML = require("gulp-minify-html");
-var imagemin = require('gulp-imagemin');
-var http = require('http');
-var cloudflare = require('cloudflare').createClient(config.cloudflareAccount);
-var less = require('gulp-less');
+var imagemin = require("gulp-imagemin");
+var http = require("http");
+var cloudflare = require("cloudflare").createClient(config.cloudflareAccount);
+var less = require("gulp-less");
 
 var modes = {
-  prod: { minify: true},
-  dev: { minify: false}
+  prod: { minify: true },
+  dev: { minify: false }
 };
 
-var modeSelect = args.mode || 'dev';
+var modeSelect = args.mode || "dev";
 var mode = modes[modeSelect];
 
 console.log("Using mode " + modeSelect, mode);
 
-gulp.task('imagemin', function() {
-  return gulp.src(config.paths.img, {
-      base: './'
+gulp.task("imagemin", function() {
+  return gulp
+    .src(config.paths.img, {
+      base: "./"
     })
-    .pipe(imagemin({
-      progressive: true
-    }))
-    .pipe(gulp.dest('./'));
+    .pipe(
+      imagemin({
+        progressive: true
+      })
+    )
+    .pipe(gulp.dest("./"));
 });
 
-gulp.task('clean', function(cb) {
+gulp.task("clean", function(cb) {
   // rimraf(config.paths.build, cb);
-  return gulp.src(config.paths.build, {
+  return gulp
+    .src(config.paths.build, {
       read: false
     }) // much faster
     .pipe(rimraf());
 });
 
-gulp.task('upload', function() {
-  return gulp.src(config.paths.build)
-    .pipe(ghPages(config.ghPages));
+gulp.task("upload", function() {
+  return gulp.src(config.paths.build).pipe(ghPages(config.ghPages));
 });
 
-gulp.task('deploy', ['build'], function() {
-  if (modeSelect !== 'prod')
-    throw "You can only deploy in production mode";
+gulp.task("deploy", ["build"], function() {
+  if (modeSelect !== "prod") throw "You can only deploy in production mode";
 
-  return runSequence(['html', 'css'], 'upload', 'purge-online-cache', 'submit-sitemap');
+  return runSequence(
+    ["html", "css"],
+    "upload",
+    "purge-online-cache",
+    "submit-sitemap"
+  );
 });
 
-gulp.task('submit-sitemap', function(cb) {
-  require('submit-sitemap').submitSitemap(config.sitemapUrl, function(err) {
-    if (err)
-      console.warn(err);
+gulp.task("submit-sitemap", function(cb) {
+  require("submit-sitemap").submitSitemap(config.sitemapUrl, function(err) {
+    if (err) console.warn(err);
 
     cb();
   });
 });
 
 // Purges website cache so updates are shown
-gulp.task('purge-online-cache', function() {
+gulp.task("purge-online-cache", function() {
   cloudflare.clearCache(config.siteDomain, 1, function() {});
 });
 
 gulp.task("html", function() {
   // Overwrite original files
-  return gulp.src(config.paths.html.src, {
-      base: './'
+  return gulp
+    .src(config.paths.html.src, {
+      base: "./"
     })
     .pipe(gulpIf(mode.minify, minifyHTML()))
-    .pipe(gulp.dest('./'));
+    .pipe(gulp.dest("./"));
 });
 
 gulp.task("css", function() {
-  return gulp.src("css/*.css", {
-      base: './'
+  return gulp
+    .src("css/*.css", {
+      base: "./"
     })
-    .pipe(less().on('error', function (err) {
-      console.log(err);
-    }))
+    .pipe(
+      less().on("error", function(err) {
+        console.log(err);
+      })
+    )
     .pipe(gulpIf(mode.minify, minifyCSS()))
-    .pipe(concat('styles.css'))
-    .pipe(gulp.dest('_site/css/'));
+    .pipe(concat("styles.css"))
+    .pipe(gulp.dest("_site/css/"));
 });
 
-gulp.task('js', function() {
-  return gulp.src('js/*.js')
-  .pipe(gulpIf(mode.minify, uglify()))
-  .pipe(concat('scripts.js'))
-  .pipe(gulp.dest('_site/js/'));
+gulp.task("js", function() {
+  return gulp
+    .src("js/*.js")
+    .pipe(gulpIf(mode.minify, uglify()))
+    .pipe(concat("scripts.js"))
+    .pipe(gulp.dest("_site/js/"));
 });
 
 // Compile SASS & auto-inject into browsers
-gulp.task('sass', function() {
-  return gulp.src('scss/styles.scss')
-    .pipe(sass({
-      includePaths: ['scss']
-    }))
-    .pipe(gulp.dest('css'))
-    .pipe(browserSync.reload({
-      stream: true
-    }));
+gulp.task("sass", function() {
+  return gulp
+    .src("scss/styles.scss")
+    .pipe(
+      sass({
+        includePaths: ["scss"]
+      })
+    )
+    .pipe(gulp.dest("css"))
+    .pipe(
+      browserSync.reload({
+        stream: true
+      })
+    );
 });
 
 // Reload all Browsers
-gulp.task('bs-reload', function() {
+gulp.task("bs-reload", function() {
   browserSync.reload();
 });
 
 function jekyllBuild(done) {
-  return require('child_process', done).exec('jekyll build --trace', {
-    stdio: 'inherit'
-  }, done);
+  return require("child_process", done).exec(
+    "jekyll build --trace",
+    {
+      stdio: "inherit"
+    },
+    done
+  );
 }
 
-gulp.task('commit-and-deploy', function(done) {
-  return require('child_process', done).exec('git add . && git commit -m "..." && git push && gulp deploy', {
-    stdio: 'inherit'
-  }, done);
+gulp.task("commit-and-deploy", function(done) {
+  return require("child_process", done).exec(
+    'git add . && git commit -m "..." && git push && gulp deploy',
+    {
+      stdio: "inherit"
+    },
+    done
+  );
 });
 
-gulp.task('jekyll', function(done) {
+gulp.task("jekyll", function(done) {
   jekyllBuild(done);
 });
 
 // Start the server
-gulp.task('browser-sync', function() {
+gulp.task("browser-sync", function() {
   browserSync({
     server: {
       baseDir: "./_site/"
@@ -154,18 +178,30 @@ gulp.task('browser-sync', function() {
   });
 });
 
-gulp.task('build', function(done) {
-  runSequence('jekyll', ['css', 'js'], 'bs-reload', done);
+gulp.task("build", function(done) {
+  runSequence("jekyll", ["css", "js"], "bs-reload", done);
 });
 
-gulp.task('default', ['build', 'browser-sync'], function() {
-//gulp.task('default', function() {
+gulp.task("default", ["build", "browser-sync"], function() {
+  //gulp.task('default', function() {
   //gulp.watch(config.paths.html.src, ['html']);
   //gulp.watch(config.paths.css, runSequence('jekyll', 'css', 'bs-reload'));
 
   //gulp.watch(["./js/*.js", "./**/*.html", "./css/*.css"],   runSequence('jekyll', 'css', 'bs-reload'));
-  gulp.watch(["./js/*.js", "./**/*.html", "./css/*.css", "./**/*.markdown", "./**/*.yml", "img/**/*", "!./build/**/*", "!./_site/**/*"], ['build']);
-
+  gulp.watch(
+    [
+      "./js/*.js",
+      "./_data/*.yaml",
+      "./**/*.html",
+      "./css/*.css",
+      "./**/*.markdown",
+      "./**/*.yml",
+      "img/**/*",
+      "!./build/**/*",
+      "!./_site/**/*"
+    ],
+    ["build"]
+  );
 
   //gulp.watch("./build/**/*", ['bs-reload']);
   //gulp.watch(["./js/*.js", "./**/*.html", "!./build/**/*", "!./_site/**/*"], ['jekyll-build', 'bs-reload']);
